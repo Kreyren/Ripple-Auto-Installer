290cca3eda748dfe183c0a231cdec6a4467c11d9
Jacob Hrbek <werifgx@gmail.com>
2019-08-18 12:09:12 +0000

    Finished current abstract

--- 01-KREYREN.bash
+++ 01-KREYREN.bash
@@ -36,7 +36,7 @@ else edebug() { true ; }
 fi
 if ! command -v die > /dev/null; then	die()	{
 	case $1 in
-		0)	true	;; # TRUE
+		0)	true && ([ -n "$debug" ] && edebug "Script returned true") ;; # TRUE
 		1)	false	;; # FALSE
 		# Syntax err
 		2)	([ -n "$2" ] && printf "FATAL: %s\n" "$2" 1>&2 ; exit "$1") || (printf "FATAL: Syntax error $0 $1 $2 $3 in ${FUNCNAME[0]}\n" 1>&2 ; exit "$1") ;;
@@ -44,6 +44,7 @@ if ! command -v die > /dev/null; then	die()	{
 		3)	([ -n "$2" ] && printf "FATAL: %s\n" "$2" 1>&2 ; exit "$1") || (printf "FATAL: Unable to elevate root access from $([ -n "$EUID" ] && printf "EUID ($EUID)")\n"	1>&2	;	exit "$1")	;;
 		# Custom
     wtf) printf "FATAL: Unexpected result in ${FUNCNAME[0]}" ;;
+    kreyren) printf "Killed by kreyren" ;;
 		*)	printf "FATAL: %s\n" "$2" 1>&2 ; exit "$1" ;;
 	esac
 } fi
@@ -56,11 +57,11 @@ checkroot() { # Check if executed as root, if not tries to use sudo if KREYREN v
 		return
 	elif [[ -x "$(command -v "sudo")" ]] && [ -n "$KREYREN" ]; then
 			einfo "Failed to aquire root permission, trying reinvoking with 'sudo' prefix"
-			exec sudo "$0" "$@" || die 3
+			sudo "$0" "$@" && edebug "Script has been executed with 'sudo' prefix" || die 3
 			die 0
 	elif [[ ! -x "$(command -v "sudo")" ]] && [ -n "$KREYREN" ]; then
 		einfo "Failed to aquire root permission, trying reinvoking as root user."
-		exec su -c "$0 $*" || die 3
+		exec su -c "$0 $*" && edebug "Script has been executed with 'su' prefix" || die 3
 		die 0
 	else
 		die 3
@@ -69,32 +70,31 @@ checkroot() { # Check if executed as root, if not tries to use sudo if KREYREN v
 
 action() {
 # Fetch repositories
-[ ! -e "/usr/src/lets" ] && (git clone https://zxq.co/ripple/lets.git /usr/src/ || die 1 "Unable to fetch ripple/lets") || edebug "Directory /usr/src/lets alredy exists"
-[ ! -e "/usr/src/hanayo" ] && (git clone https://zxq.co/ripple/hanayo.git /usr/src/ || die 1 "Unable to fetch ripple/hanayo") || edebug "Directory /usr/src/hanayo alredy exists"
-[ ! -e "/usr/src/rippleapi" ] && (git clone https://zxq.co/ripple/rippleapi.git /usr/src/ || die 1 "Unable to fetch ripple/rippleapi") || edebug "Directory /usr/src/rippleapi alredy exists"
-[ ! -e "/usr/src/chesegull" ] && (git clone https://zxq.co/ripple/chesegull.git /usr/src/ || die 1 "Unable to fetch ripple/chesegull") || edebug "Directory /usr/src/chesegull alredy exists"
-[ ! -e "/usr/src/avatar-server-go" ] && (git clone https://zxq.co/Sunpy/avatar-server-go.git /usr/src/ || die 1 "Unable to fetch Sunpy/avatar-server-go") || edebug "Directory /usr/src/lets alredy exists"
+[ ! -e "/usr/src/lets" ] && (git clone https://zxq.co/ripple/lets.git /usr/src/lets || die 1 "Unable to fetch ripple/lets") || edebug "Directory /usr/src/lets alredy exists"
+[ ! -e "/usr/src/hanayo" ] && (git clone https://zxq.co/ripple/hanayo.git /usr/src/hanayo || die 1 "Unable to fetch ripple/hanayo") || edebug "Directory /usr/src/hanayo alredy exists"
+[ ! -e "/usr/src/rippleapi" ] && (git clone https://zxq.co/ripple/rippleapi.git /usr/src/rippleapi || die 1 "Unable to fetch ripple/rippleapi") || edebug "Directory /usr/src/rippleapi alredy exists"
+[ ! -e "/usr/src/chesegull" ] && (git clone https://zxq.co/ripple/chesegull.git /usr/src/chesegull || die 1 "Unable to fetch ripple/chesegull") || edebug "Directory /usr/src/chesegull alredy exists"
+[ ! -e "/usr/src/avatar-server-go" ] && (git clone https://zxq.co/Sunpy/avatar-server-go.git /usr/src/avatar-server-go || die 1 "Unable to fetch Sunpy/avatar-server-go") || edebug "Directory /usr/src/lets alredy exists"
 }
 
 # LOGIC
 
 checkroot "$@" && while [[ "$#" -gt 0 ]]; do case "$1" in
-	# TODO: Capture $1 and $2 for MFH
-	-mfh|--make-file-hierarchy|-MFH)
-		if [[ "$2" != -* ]] && [[ "$3" != -* ]]; then
-			MFH "$2" "$3"
+  --action)
+    if [[ "$2" != -* ]] && [[ "$3" != -* ]]; then
+			action "$2" "$3"
 			shift 3
 		elif [[ "$2" != -* ]] && [[ "$3" == -* ]]; then
-			MFH "$1"
+			action "$1"
 			shift 1
 		elif [[ "$2" == -* ]] && [[ "$3" == -* ]]; then
+      action
 			shift 1
-		else die "Unexpected result in -mfh logic"
+		else die "Unexpected result in -action logic"
 		fi
-	;;
-  -action) action1 ; action ;;
+  ;;
 	-d|--debug) debug="true" ; shift ;;
 	-h|--help) printf "STUB: HELP_PAGE" ;;
-	"") die 0 ;; # Needed to output success
+	"") action ; die 0 ;; # Needed to output success
 	*) die 2 ; break
 esac; done
