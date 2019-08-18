da8bf75cb3d8cf57b52e97d6e21fb908c5c73898
Jacob Hrbek <werifgx@gmail.com>
2019-08-18 11:08:58 +0000

    Replaced temporary sudo wrapper with checkroot()

--- 00-ripple-api.bash
+++ 00-ripple-api.bash
@@ -2,8 +2,23 @@
 # Ripple api written in bash
 # Created by github.com/kreyren under the terms of GPL-2 (https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)
 
-# SUDO wrapper
-sudo() { [ -x $(command -v "sudo") ] && printf "${EUID:+sudo}" ; }
+# CHECKROOT
+checkroot() { # Check if executed as root, if not tries to use sudo if KREYREN variable is not blank
+  # Licenced by github.com/kreyren under GPL-2
+	if [[ "$EUID" == 0 ]]; then
+		return
+	elif [[ -x "$(command -v "sudo")" ]] && [ -n "$KREYREN" ]; then
+			einfo "Failed to aquire root permission, trying reinvoking with 'sudo' prefix"
+			exec sudo "$0" "$@" || die 3
+			die 0
+	elif [[ ! -x "$(command -v "sudo")" ]] && [ -n "$KREYREN" ]; then
+		einfo "Failed to aquire root permission, trying reinvoking as root user."
+		exec su -c "$0 $*" || die 3
+		die 0
+	else
+		die 3
+	fi
+}
 
 # Error handling
 if ! command -v "einfo" > /dev/null; then einfo() { printf "INFO: %s\n" "${*}" 1>&2 ; } ; fi
