1390868e6723e7997c676bfcbeb2e35841305f67
Jacob Hrbek <werifgx@gmail.com>
2019-08-20 17:28:44 +0000

    Fixed die() and detection for mysql_config which is required for lets

--- 01-KREYREN.bash
+++ 01-KREYREN.bash
@@ -34,28 +34,39 @@
 #### pip install -r requirements.txt
 ##### pip reguired (DEBIAN: python3-pip)
 
-
-
 # GLOBAL
 
 # Sanitization for API used
 # [ -e "/tmp/00-ripple-api.bash" ] && (source "/tmp/00-ripple-api.bash" || die 1 "Unable to fetch ripple API") || warn "Unable to source ripple-api, trying to fetch" && (wget "https://raw.githubusercontent.com/Kreyren/Ripple-Auto-Installer/kreyrenizing/00-ripple-api.bash" -O "/tmp/00-ripple-api.bash" || die 1 "Unable to fetch ripple-api") && (source "/tmp/00-ripple-api.bash" && einfo "ripple-api was fetched and sourced" || die 1 "Failed to source ripple-api")
 
 # Error handling
-if ! command -v einfo > /dev/null; then	einfo()	{	printf "INFO: %s\n" "$1"	1>&2	;	} fi
-if ! command -v warn > /dev/null; then	warn()	{	printf "WARN: %s\n" "$1"	1>&2	;	} fi
+if ! command -v "einfo" > /dev/null; then	einfo()	{	printf "INFO: %s\n" "$1"	1>&2	;	} fi
+if ! command -v "warn" > /dev/null; then	warn()	{	printf "WARN: %s\n" "$1"	1>&2	;	} fi
 if [ -n "$debug" ]; then
 	edebug()	{	printf "DEBUG: %s\n" "$1"	1>&2	; }
 else edebug() { true ; }
 fi
-if ! command -v die > /dev/null; then	die()	{
+if ! command -v "die" > /dev/null; then	die()	{
 	case $1 in
-		0|true)	([ -n "$debug" ] && edebug "Script returned true") ; exit "$1" ;; # TRUE
-		1|false)  ([ -n "$2" ] && printf "FATAL: %s\n" "$2" 1>&2 ; exit "$1") || (printf "FATAL: Script returned false $([ -n "$EUID" ] && printf "EUID ($EUID)") ${FUNCNAME[0]}\n" 1>&2 ; exit "$1") ;;
-		# Syntax err
-		2)	([ -n "$2" ] && printf "FATAL: %s\n" "$2" 1>&2 ; exit "$1") || (printf "FATAL: Syntax error $([ -n "$debug" ] && printf "$0 $1 $2 $3 in ${FUNCNAME[0]}")\n" 1>&2 ; exit "$1") ;;
-		# Permission issue
-		3)	([ -n "$2" ] && printf "FATAL: %s\n" "$2" 1>&2 ; exit "$1") || (printf "FATAL: Unable to elevate root access from $([ -n "$EUID" ] && printf "EUID ($EUID)")\n"	1>&2	;	exit "$1")	;;
+		0|true)	([ -n "$debug" ] && edebug "Script returned true") ; exit "$1" ;;
+		1|false) # False
+			if [ -n "$2" ]; then printf "FATAL: %s\n" "$2" 1>&2 ; exit "$1"
+			elif [ -z "$2" ] ; then printf "FATAL: Script returned false $([ -n "$EUID" ] && printf "from EUID ($EUID)") ${FUNCNAME[0]}\n" 1>&2 ; exit "$1"
+			else die wtf
+			fi
+		;;
+		2) # Syntax err
+			if [ -n "$2" ]; then printf "FATAL: %s\n" "$2" 1>&2 ; exit "$1"
+			elif [ -z "$2" ]; then printf "FATAL: Syntax error $([ -n "$debug" ] && printf "$0 $1 $2 $3 in ${FUNCNAME[0]}")\n" 1>&2 ; exit "$1"
+			else die wtf
+			fi
+		;;
+		3) # Permission issue
+		if [ -n "$2" ]; then printf "FATAL: %s\n" "$2" 1>&2 ; exit "$1"
+		elif [ -z "$2" ]; then printf "FATAL: Unable to elevate root access from $([ -n "$EUID" ] && printf "EUID ($EUID)")\n" 1>&2	;	exit "$1"
+		else die wtf
+		fi
+		;;
 		# Custom
     wtf) printf "FATAL: Unexpected result in ${FUNCNAME[0]}\n" ;;
     kreyren) printf "Killed by kreyren\n" ;;
@@ -90,10 +101,10 @@ action() {
   [ ! -e "/usr/src/avatar-server-go" ] && (git clone https://zxq.co/Sunpy/avatar-server-go.git /usr/src/avatar-server-go || die 1 "Unable to fetch Sunpy/avatar-server-go") || edebug "Directory /usr/src/lets alredy exists"
   [ ! -e "/usr/src/pep.py" ] && (git clone https://zxq.co/ripple/pep.py.git /usr/src/pep.py || die 1 "Unable to fetch Sunpy/pep.py") || edebug "Directory /usr/src/lets alredy exists"
 
-  # STUB: investigate
-  if ! command -v mysql_config >/dev/null; then die 1 "Command 'mysql_config' is not executable" ; fi
-
-  return
+  # Required for lets
+  if ! command -v mysql_config >/dev/null; then die 1 "Command 'mysql_config' is not executable\n"
+	#TODO: elif grep -qF "Debian" "/etc/os-release" && [ -n "$KREYREN" ]; then	einfo "This package depends on mysql_config from libmariadb-dev-compat on Debian which will be installed now" ; apt install libmariadb-dev-compat -y
+	fi
 
   pip3 install -r /usr/src/lets/requirements.txt && edebug "pip3 returned true for /usr/src/lets/requirements.txt" || die "pip3 failed to fetch required packages"
 }
@@ -101,8 +112,8 @@ action() {
 # LOGIC
 
 checkroot "$@" && while [[ "$#" -ge 0 ]]; do case "$1" in
-	-h|-\?|--help) printf "STUB: HELP_PAGE" && break ;;
-	-d|--debug) export debug="true" ; shift 1 ;;
+	-h|-\?|--help) printf "STUB: HELP_PAGE" && break ;; # TODO: Sanitize on variables
+	-d|--debug) export debug="true" ; shift 1 ;; # TODO: Sanitize on variables
 	"") action ;;
 	*) die 2 ; break
 esac; done
